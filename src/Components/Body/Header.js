be107024feccafcbec59a51afc6d641a818ec255
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2022-02-15 21:04:12 +0530

    Finished header and navbar

--- src/Components/Body/Header.js
+++ src/Components/Body/Header.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import Typed from 'react-typed';
 
 export const Header = () => {
-  return (
-    <>
-        <div className="header-wrapper" >
-            <div className="main-info">
-                <h1>Our Products</h1>
+    return (
+        <>
+            <div className="header-wrapper" >
+                <div className="main-info">
+                    <h1 className='main-text'>Our Special Products</h1>
+                    <div>
+                        <Typed
+                            className='typed-text'
+                            strings={['Model S',
+                                'Model 3',
+                                'Model X',
+                                'Model Y',
+                                'Solar Roof',
+                                'Solar Panels'
+                            ]}
+                            typeSpeed={90}
+                            backSpeed={80}
+                            loop >
+                        </Typed>
+                    </div>
+                </div>
             </div>
-            
-        </div>
-    </>
-  )
+        </>
+    )
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React, { useRef } from 'react'
 import logo from '../Tesla.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisV, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
-    const navBtn = useRef(null)
+    const navBtn = useRef(null);
+    const overlay = useRef(null);
 
     const handleDropdown = e => {
         navBtn.current.click();
@@ -13,82 +14,108 @@ export const Navbar = () => {
     const handleUp = e => {
         navBtn.current.click();
     }
+
+    const openNav = () => {
+        overlay.current.style.width = '100%';
+    }
+
+    const closeNav = () => {
+        overlay.current.style.width = '0%';
+    }
+
     return (
         <>
-            <nav class="navbar navbar-expand-lg fixed-top navbar-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src={logo} alt="logo" class="nav-logo mx-5 my-3"></img></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <FontAwesomeIcon icon={faEllipsisV} className='navbar-icon'/>
+            <nav className="navbar navbar-expand-lg fixed-top navbar-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src={logo} alt="logo" className="nav-logo mx-5 my-3"></img></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <FontAwesomeIcon icon={faEllipsisV} className='navbar-icon' />
                     </button>
-                    <div class="collapse navbar-collapse flex-right" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mx-3 list1">
-                        <li className="nav-item">
-                                 <div class="dropdown">
-                                    <button class="button dropdown-toggle my-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" ref={navBtn}>
+                    <div className="collapse navbar-collapse flex-right" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mx-3 list1">
+                            <li className="nav-item">
+                                <div className="dropdown">
+                                    <button className="button dropdown-toggle my-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" ref={navBtn}>
                                         Products
                                     </button>
-                                    <a class="nav-link active dropdown-text" onMouseEnter={handleDropdown} onMouseLeave={handleUp}>Products</a>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" onMouseEnter={handleDropdown} onMouseLeave={handleUp}>
-                                        <li><a class="dropdown-item" href="#">Model S</a></li>
-                                        <li><a class="dropdown-item" href="#">Model 3</a></li>
-                                        <li><a class="dropdown-item" href="#">Model X</a></li>
-                                        <li><a class="dropdown-item" href="#">Model Y</a></li>
-                                        <li><a class="dropdown-item" href="#">Solar Roof</a></li>
-                                        <li><a class="dropdown-item" href="#">Solar Panels</a></li>
+                                    <a className="nav-link active dropdown-text" onMouseEnter={handleDropdown} onMouseLeave={handleUp}>Products</a>
+                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1" onMouseEnter={handleDropdown} onMouseLeave={handleUp}>
+                                        <li><a className="dropdown-item" href="#">Model S</a></li>
+                                        <li><a className="dropdown-item" href="#">Model 3</a></li>
+                                        <li><a className="dropdown-item" href="#">Model X</a></li>
+                                        <li><a className="dropdown-item" href="#">Model Y</a></li>
+                                        <li><a className="dropdown-item" href="#">Solar Roof</a></li>
+                                        <li><a className="dropdown-item" href="#">Solar Panels</a></li>
                                     </ul>
                                 </div>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Shop</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Shop</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Account</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Account</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Support</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" onClick={openNav} style={{cursor: 'pointer'}}>Menu</a>
                             </li>
                         </ul>
-                        <ul class="navbar-nav ms-auto mx-3 list2">
-                        <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Model S</a>
+
+                        {/***************Curtain Menu for Computers*************** */}
+                        <div id="Menu" className="overlay" ref={overlay}>
+                            <FontAwesomeIcon icon={faCircleXmark} className='closebtn' onClick={closeNav}/>
+
+                            <div class="overlay-content">
+                                <a href="#">Existing Inventory</a>
+                                <a href="#">Used Inventory</a>
+                                <a href="#">Trade In</a>
+                                <a href="#">Test Drive</a>
+                                <a href="#">Cybertruck</a>
+                                <a href="#">Roadstar</a>
+                                <a href="#">Utilities</a>
+                                <a href="#">Find Us</a>
+                            </div>
+                        </div>
+
+                        <ul className="navbar-nav ms-auto mx-3 list2">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Model S</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Model 3</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Model 3</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Model X</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Model X</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Model Y</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Model Y</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Solar Roof</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Solar Roof</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Solar Panels</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Solar Panels</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Existing Inventory</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Existing Inventory</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Used Inventory</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Used Inventory</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Trade-in</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Trade-in</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Test Drive</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Test Drive</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Shop</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Shop</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Account</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Account</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#">Support</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Support</a>
                             </li>
                         </ul>
                     </div>
